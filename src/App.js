67b98983c5e8f4c0a60a67a27afbeeeb42e45f02
kevin cheung <kevin.cheung.kai@gmail.com>
2022-12-06 00:17:47 -0500

    implemented random coinflip by using state and passing down random number to coinflip component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import CoinFlip from "./components/CoinFlip";
 
 const App = () => {
-  return <CoinFlip />;
+  const [counter, setCounter] = useState(0);
+  const [number, setNumber] = useState(0);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+    setNumber(Math.floor(Math.random() * 2));
+  };
+
+  console.log(number);
+  return (
+    <div>
+      <CoinFlip number={number} />
+      <button onClick={handleClick}>Flip Me</button>
+      <div>{`Out of ${counter} flips`}</div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/CoinFlip.js
+++ src/components/CoinFlip.js
@@ -1,5 +1,13 @@
-const CoinFlip = () => {
-  return <div>Coinflip</div>;
+const CoinFlip = ({ number }) => {
+  return (
+    <div>
+      {number == 1 ? (
+        <img src="https://tinyurl.com/react-coin-heads-jpg" />
+      ) : (
+        <img src="https://media.istockphoto.com/id/476142091/photo/quarter-dollar-us-coin-isolated-on-white.jpg?s=612x612&w=0&k=20&c=wNzr7m0Z3dhlf8_O1G3EFNz8u2tALVobVs4K4XfFN5c=" />
+      )}
+    </div>
+  );
 };
 
 export default CoinFlip;
